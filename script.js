bc3c5e0c994bbe895eb11cf95eed23aaccdeda97
RenanBarth <renan.barth@escola.pr.gov.br>
2026-08-06 08:37:49 -0300

    gg

--- script.js
+++ script.js
@@ -5,11 +5,18 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 
 const pergunta = [
+   {
    enunciado: "Pergunta 1",
    alternativas: [
-    "Alternativa 1", 
-    "Alternativa 2"
-]
-
-    "Pergunta 2"
+       "Alternativa 1", 
+       "Alternativa 2"
+    ]
+},
+{
+   enunciado: "Pergunta 1",
+   alternativas: [
+       "Alternativa 1", 
+       "Alternativa 2"
+    ]
+}
 ]
